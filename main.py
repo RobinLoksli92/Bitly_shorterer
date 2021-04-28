9299d43bcf285db375021b3b49f9e0185d0244cb
RobinLoksli92 <gleb11tiun@mail.ru>
2021-04-28 11:27:16 +0300

    Улучешния. Доработки.

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
-def createParser():
+def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('site', nargs='?')
+    parser.add_argument('site', ) # !
     return parser
 
 
 def count_clicks(headers, url_for_clicks_count):
-    clicks_count = requests.get(url_for_clicks_count, headers=headers)
-    clicks_count.raise_for_status()
-    clicks_count = clicks_count.json()
-    return clicks_count
+    response = requests.get(url_for_clicks_count, headers=headers)
+    response.raise_for_status()
+    response = response.json()
+    return response
 
 
 def shorten_link(headers, url_for_short_links, payload):
@@ -26,17 +26,17 @@ def shorten_link(headers, url_for_short_links, payload):
 
 
 def check_the_link_for_short(headers, url_for_bitlink_info):
-    bitlink_info = requests.get(url_for_bitlink_info, headers=headers)
-    bitlink_info = bitlink_info.ok
-    return bitlink_info
+    response = requests.get(url_for_bitlink_info, headers=headers)
+    response = response.ok
+    return response
 
 
 def main():
-    parser = createParser()
+    parser = create_parser()
     namespace = parser.parse_args()
-    link = namespace.site
+    link_from_user = namespace.site
     url_for_short_links = 'https://api-ssl.bitly.com/v4/shorten'
-    parsed_link_from_user = urlparse(link)
+    parsed_link_from_user = urlparse(link_from_user)
     link_to_check_for_short = '{}{}'.format(parsed_link_from_user.netloc, parsed_link_from_user.path)
     url_for_bitlink_info = 'https://api-ssl.bitly.com/v4/bitlinks/{}'.format(link_to_check_for_short)
 
@@ -44,7 +44,7 @@ def main():
         'Authorization': os.getenv('BITLY_TOKEN')
     }
     payload = {
-        'long_url': link
+        'long_url': link_from_user
     }
 
     short_link_check = check_the_link_for_short(headers, url_for_bitlink_info)
@@ -66,6 +66,6 @@ def main():
             print('Ошибка!!!!!!')
         
        
-if __name__ == "__main__":
+if __name__ == '__main__':
     load_dotenv()
     main()
